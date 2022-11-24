00605b31568f246e865448ad86f9f1548b49a2cc
Ibrahim <ibbi2k2.khan@gmail.com>
2022-11-24 23:29:02 +0500

    feat: add level one layer procs

--- game.asm
+++ game.asm
@@ -5,6 +5,9 @@
 include include\vars.inc
 
 .code
+
+include include\macros.inc
+
 mov ax, @data
 mov ds, ax
 
@@ -12,9 +15,55 @@ main proc
 call setVideoMode
 call setBackgroundColor
 
-mov cx, 270
-mov dx, 150
-call drawBrick
+mov cx, 20
+mov dx, 30
+call drawLevelOneLayers
+
+; mov cx, 270
+; mov dx, 150
+; call drawBrick
+
+; CheckKeys:
+; mov ah, 00
+; int 16h
+; jnz CheckKeys
+; cmp ah, 72
+; je drawUp
+; cmp ah, 77
+; je drawRight
+; cmp ah, 80
+; je drawDown
+; cmp ah, 75
+; je drawLeft
+; cmp ah, 1
+; je exit
+; drawUp:
+; call clearScreen
+; dec dx
+; call drawBrick
+; ; mov al, backgroundColor
+; ; mov bh, brickWidth
+; ; mov bl, 1
+; ; push dx
+; ; add dl, brickHeight
+; ; call drawRectangle
+; ; pop dx
+; jmp CheckKeys
+; drawRight:
+; call clearScreen
+; inc cx
+; call drawBrick
+; jmp CheckKeys
+; drawDown:
+; call clearScreen
+; inc dx
+; call drawBrick
+; jmp CheckKeys
+; drawLeft:
+; call clearScreen
+; dec cx
+; call drawBrick
+; jmp CheckKeys
 
 jmp exit
 main endp
