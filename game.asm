88f59345d96b7e6f68da229c5af79b586d3d7abb
Ibrahim <ibbi2k2.khan@gmail.com>
2022-11-25 11:38:06 +0500

    feat: add pad proc

--- game.asm
+++ game.asm
@@ -18,52 +18,40 @@ call setBackgroundColor
 mov cx, 20
 mov dx, 30
 call drawLevelOneLayers
+mov cx, 120
+mov dx, 190
+call drawPad
 
-; mov cx, 270
-; mov dx, 150
-; call drawBrick
-
-; CheckKeys:
-; mov ah, 00
-; int 16h
-; jnz CheckKeys
-; cmp ah, 72
-; je drawUp
-; cmp ah, 77
-; je drawRight
-; cmp ah, 80
-; je drawDown
-; cmp ah, 75
-; je drawLeft
-; cmp ah, 1
-; je exit
-; drawUp:
-; call clearScreen
-; dec dx
-; call drawBrick
-; ; mov al, backgroundColor
-; ; mov bh, brickWidth
-; ; mov bl, 1
-; ; push dx
-; ; add dl, brickHeight
-; ; call drawRectangle
-; ; pop dx
-; jmp CheckKeys
-; drawRight:
-; call clearScreen
-; inc cx
-; call drawBrick
-; jmp CheckKeys
-; drawDown:
-; call clearScreen
-; inc dx
-; call drawBrick
-; jmp CheckKeys
-; drawLeft:
-; call clearScreen
-; dec cx
-; call drawBrick
-; jmp CheckKeys
+CheckKeys:
+mov ah, 00
+int 16h
+jnz CheckKeys
+cmp ah, 72
+je drawUp
+cmp ah, 77
+je drawRight
+cmp ah, 80
+je drawDown
+cmp ah, 75
+je drawLeft
+cmp ah, 1
+je exit
+drawUp:
+dec dx
+call drawPad
+jmp CheckKeys
+drawRight:
+inc cx
+call drawPad
+jmp CheckKeys
+drawDown:
+inc dx
+call drawPad
+jmp CheckKeys
+drawLeft:
+dec cx
+call drawPad
+jmp CheckKeys
 
 jmp exit
 main endp
