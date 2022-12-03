9eb4a27bbde79b475395d00634c6f72ccebb9ff5
Ibrahim <ibbi2k2.khan@gmail.com>
2022-12-03 15:46:13 +0500

    add hud

--- game.asm
+++ game.asm
@@ -14,7 +14,7 @@ mov ds, ax
 main proc
 ; call setBackgroundColor
 againMenu:
-call setVideoMode
+setVideoMode
 call menu
 call choice         ;Choosing 1 of the 3 Options given in Menu
 cmp MenuChoice,1
@@ -23,53 +23,55 @@ JZ againMenu
 setVideoMode
 setBackgroundColor
 
+enableHUD
+
 setupLevelOne
 
-mov si, offset brickCoordinates
-mov bx, 0
-looop:
-push bx
-call checkHalfBrick
-.IF bx == 1
-    pop bx
-    push bx
-    mov ax, bx
-    mov bh, 0
-    mov bl, 4
-    mul bl
-    mov bx, ax
-    mov cx, [si + bx]
-    add bx, 2
-    mov dx, [si + bx]
-    mov ax, 0
-    mov al, brickWidth
-    mov bh, 0
-    mov bl, 2
-    div bl
-    mov bh, al
-    mov bl, brickHeight
-    mov al, 2fh
-    call drawRectangle
-.ELSE
-    pop bx
-    push bx
-    mov ax, bx
-    mov bh, 0
-    mov bl, 4
-    mul bl
-    mov bx, ax
-    mov cx, [si + bx]
-    add bx, 2
-    mov dx, [si + bx]
-    mov bh, brickWidth
-    mov bl, brickHeight
-    mov al, 36h
-    call drawRectangle
-.ENDIF
-pop bx
-inc bx
-cmp bx, ((brickLayerCount * levelLayers) + (levelLayers / 2))
-jb looop
+; mov si, offset brickCoordinates
+; mov bx, 0
+; looop:
+; push bx
+; call checkHalfBrick
+; .IF bx == 1
+;     pop bx
+;     push bx
+;     mov ax, bx
+;     mov bh, 0
+;     mov bl, 4
+;     mul bl
+;     mov bx, ax
+;     mov cx, [si + bx]
+;     add bx, 2
+;     mov dx, [si + bx]
+;     mov ax, 0
+;     mov al, brickWidth
+;     mov bh, 0
+;     mov bl, 2
+;     div bl
+;     mov bh, al
+;     mov bl, brickHeight
+;     mov al, 2fh
+;     call drawRectangle
+; .ELSE
+;     pop bx
+;     push bx
+;     mov ax, bx
+;     mov bh, 0
+;     mov bl, 4
+;     mul bl
+;     mov bx, ax
+;     mov cx, [si + bx]
+;     add bx, 2
+;     mov dx, [si + bx]
+;     mov bh, brickWidth
+;     mov bl, brickHeight
+;     mov al, 36h
+;     call drawRectangle
+; .ENDIF
+; pop bx
+; inc bx
+; cmp bx, ((brickLayerCount * levelLayers) + (levelLayers / 2))
+; jb looop
 
 mov bh, 0
 mov bl, ballSpeed
