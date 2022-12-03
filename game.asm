983951ebf0364b6d69503eefe153370621f0dc39
unknown <zaain.rizwan@gmail.com>
2022-12-03 14:16:12 +0500

    create menu

--- game.asm
+++ game.asm
@@ -12,46 +12,55 @@ mov ax, @data
 mov ds, ax
 
 main proc
+
+; call setBackgroundColor
+againMenu:
 call setVideoMode
-call setBackgroundColor
+call menu
+call choice         ;Choosing 1 of the 3 Options given in Menu
+cmp MenuChoice,1
+JZ againMenu
 
-mov cx, 20
-mov dx, 30
-call drawLevelOneLayers
-mov cx, 120
-mov dx, 190
-call drawPad
+call NewScreen ;Set Background Again
+call WinScreen
 
-CheckKeys:
-mov ah, 00
-int 16h
-jnz CheckKeys
-cmp ah, 72
-je drawUp
-cmp ah, 77
-je drawRight
-cmp ah, 80
-je drawDown
-cmp ah, 75
-je drawLeft
-cmp ah, 1
-je exit
-drawUp:
-dec dx
-call drawPad
-jmp CheckKeys
-drawRight:
-inc cx
-call drawPad
-jmp CheckKeys
-drawDown:
-inc dx
-call drawPad
-jmp CheckKeys
-drawLeft:
-dec cx
-call drawPad
-jmp CheckKeys
+; mov cx, 20
+; mov dx, 30
+; call drawLevelOneLayers
+; mov cx, 120
+; mov dx, 190
+; call drawPad
+
+; CheckKeys:
+; mov ah, 00
+; int 16h
+; jnz CheckKeys
+; cmp ah, 72
+; je drawUp
+; cmp ah, 77
+; je drawRight
+; cmp ah, 80
+; je drawDown
+; cmp ah, 75
+; je drawLeft
+; cmp ah, 1
+; je exit
+; drawUp:
+; dec dx
+; call drawPad
+; jmp CheckKeys
+; drawRight:
+; inc cx
+; call drawPad
+; jmp CheckKeys
+; drawDown:
+; inc dx
+; call drawPad
+; jmp CheckKeys
+; drawLeft:
+; dec cx
+; call drawPad
+; jmp CheckKeys
 
 jmp exit
 main endp
